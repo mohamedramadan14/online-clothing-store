4497cbed83f02cbe8cfe76b5ccf4a7079f28633b
Mohamed Ramadan <mohameda7maa@gmail.com>
2022-08-03 22:19:46 +0200

    Adding Navigation starter and Directory components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Route, Routes } from "react-router-dom";
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
